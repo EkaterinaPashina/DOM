599175af842c0a9c4a1262dd35b92fad373ce66a
EkaterinaPashina <k377at@mail.ru>
2023-05-13 23:45:09 +0500

    added functions to js

---
+++ dom.js
@@ -0,0 +1,14 @@
+const elems = document.querySelectorAll('.card__price_count');
+const firstPrice = Number((elems[0].innerText).slice(0, -1));
+const secondPrice = Number((elems[1].innerText).slice(0, -1));
+const thirdPrice = Number((elems[2].innerText).slice(0, -1));
+const fourthPrice = Number((elems[3].innerText).slice(0, -1));
+const result = firstPrice + secondPrice + thirdPrice + fourthPrice;
+
+const paragraph = document.querySelector('#basket__total__result');
+paragraph.textContent = String(result) + 'р';
+
+const myFunction = () => {
+    const paragraph = document.querySelector('#basket__total__result');
+    paragraph.textContent = String(result / 100 * 80) + 'р';
+};
